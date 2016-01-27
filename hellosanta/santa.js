93e2e35544db0f2d289dec6ca3ab712bdd79807c
jimmy8646 <yujiechueh@gmail.com>
2016-01-27 19:12:54 +0800

    popup 日期

--- hellosanta/santa.js
+++ hellosanta/santa.js
@@ -1,12 +1,35 @@
 /**
  * Created by cobenash on 16/1/26.
  */
-(function ($) {
+function select_date(date) {
+    var day = date.getDay();
+    if (day == 1 || day == 0) {
+
+        return [false];
+
+    } else {
+
+        return [true];
+    }
+
+}
+(function($) {
+    //var id = $("#commerce-shipping-service-details input").attr('id');
+var clickwhat1 = $('#edit-commerce-shipping-shipping-service-deliver').attr('checked');
+var clickwhat2 = $('#edit-commerce-shipping-shipping-service-my-store').attr('checked');
     Drupal.behaviors.santa = {
         attach: function (context) {
             for (var id in Drupal.settings.datePopup) {
-                Drupal.settings.datePopup['edit-commerce-shipping-service-details-time-datepicker-popup-0'].settings.beforeShowDay = $.datepicker.noWeekends;
+                clickwhat1 = $('#edit-commerce-shipping-shipping-service-deliver').attr('checked');
+                clickwhat2 = $('#edit-commerce-shipping-shipping-service-my-store').attr('checked');
+                if (clickwhat1){
+                    Drupal.settings.datePopup[id].settings.minDate = +1;
+                    Drupal.settings.datePopup[id].settings.beforeShowDay = select_date;//$.datepicker.noWeekends;
+                } else {
+                     Drupal.settings.datePopup[id].settings.minDate = +1;
+                }
             }
         }
     };
+
 })(jQuery);
---
+++ hellosanta/setting.js
@@ -0,0 +1,20 @@
+/**
+ * Created by cobenash on 16/1/26.
+ */
+
+// (function($) {
+    
+function (date) {
+                    var day = date.getDay();
+                    if (day == 1 || day == 0) {
+                        return [false];
+                    } else {
+                        return [true];
+                    }
+
+                };
+                minDate = +1;
+            }
+        }
+    };
+})(jQuery);
