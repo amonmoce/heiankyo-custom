e0ed45c8e712dbad4bc00b69050a31ec392e1c25
jimmy8646 <yujiechueh@gmail.com>
2016-01-28 12:26:29 +0800

    時段bug fix

--- hellosanta/santa.js
+++ hellosanta/santa.js
@@ -4,29 +4,30 @@
 function select_date(date) {
     var day = date.getDay();
     if (day == 1 || day == 0) {
-
         return [false];
-
     } else {
-
         return [true];
     }
-
 }
 (function($) {
-    //var id = $("#commerce-shipping-service-details input").attr('id');
-var clickwhat1 = $('#edit-commerce-shipping-shipping-service-deliver').attr('checked');
-var clickwhat2 = $('#edit-commerce-shipping-shipping-service-my-store').attr('checked');
+    var clickwhat1 = $('#edit-commerce-shipping-shipping-service-deliver').attr('checked');
+    var clickwhat2 = $('#edit-commerce-shipping-shipping-service-my-store').attr('checked');
+    $(document).ready(function() {
+        if (clickwhat1) {
+            jQuery("#edit-commerce-shipping-service-details-timing option[value='4']").remove();
+        }
+    });
     Drupal.behaviors.santa = {
-        attach: function (context) {
+        attach: function(context) {
             for (var id in Drupal.settings.datePopup) {
                 clickwhat1 = $('#edit-commerce-shipping-shipping-service-deliver').attr('checked');
                 clickwhat2 = $('#edit-commerce-shipping-shipping-service-my-store').attr('checked');
-                if (clickwhat1){
+                if (clickwhat1) {
                     Drupal.settings.datePopup[id].settings.minDate = +1;
-                    Drupal.settings.datePopup[id].settings.beforeShowDay = select_date;//$.datepicker.noWeekends;
+                    Drupal.settings.datePopup[id].settings.beforeShowDay = select_date;
+                    jQuery("#edit-commerce-shipping-service-details-timing option[value='4']").remove();
                 } else {
-                     Drupal.settings.datePopup[id].settings.minDate = +1;
+                    Drupal.settings.datePopup[id].settings.minDate = +1;
                 }
             }
         }
